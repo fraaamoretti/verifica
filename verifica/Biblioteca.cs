84ef52a00a8c703d7c0ea1249786e510e75c5b92
Francesco Moretti <FRANCESCO.MORETTI@local.ittcesena.it>
2022-11-12 08:59:41 +0100

    biblioteca - parte 1

---
+++ verifica/Biblioteca.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verifica
+{
+    class Biblioteca
+    {
+        string nome, indirizzo, orarioLavorativo;
+        public List<Libro> elencoLibri;
+
+        public Biblioteca(string nome, string indirizzo, string orarioLavorativo)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.orarioLavorativo = orarioLavorativo;
+            elencoLibri = new List<Libro>();
+        }
+
+        public bool aggiungiLibro(Libro libroDaAggiungere)
+        {
+            try
+            {
+                elencoLibri.Add(libroDaAggiungere);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string cercaLibro(string titolo)
+        {
+            foreach(Libro l in elencoLibri)
+            {
+                if(l.titolo == titolo)
+                {
+                    return l.toString();
+                }
+            }
+            return "il libro non è presente nell'elenco";
+        }
+    }
+}
